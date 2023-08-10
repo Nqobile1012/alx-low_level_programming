ac4689d9670cde83be59cd36eb441d3dee45465a
Nqobile1012 <buthelezinqobile45g@gmail.com>
2023-08-10 06:27:39 -0700

    Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the function that prints
+ *
+ * Return: must be 0
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
