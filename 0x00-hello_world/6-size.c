585e2d16383ec0dbf9908c0949ee82d3990e5fa7
Nqobile1012 <buthelezinqobile45g@gmail.com>
2023-08-10 07:18:11 -0700

    Size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: must be 0
+ */
+
+
+int main(void)
+{
+printf("Size of a char: %lu byte(s)", sizeof(char));
+printf("Size of an int: %lu byte(s)", sizeof(int));
+printf("Size of a long int: %lu byte(s)", sizeof(long int);
+		printf("Size of a long long int: %lu byte(s)", sizeof(long long int);
+			printf("Size of a float: %lu byte(s)", sizeof(float);
+return (0);
+}
