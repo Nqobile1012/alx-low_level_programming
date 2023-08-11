8139195a3f45fa7e4530d931aaef378fd3a2252a
Nqobile1012 <buthelezinqobile45g@gmail.com>
2023-08-11 07:35:48 -0700

    Tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry main
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i >= 'a' ; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
