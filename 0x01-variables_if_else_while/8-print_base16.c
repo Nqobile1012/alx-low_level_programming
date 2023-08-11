b5299943b78349d271f2dce576f98b09611117de
Nqobile1012 <buthelezinqobile45g@gmail.com>
2023-08-11 11:51:52 -0700

    Base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,12 +9,12 @@
 int main(void)
 {
 	int i;
-	char c;
+	char j;
 
 	for (i = 0 ; i < 10 ; i++)
 		putchar(i = '0');
-	for (c = 'a' ; c <= 'f' ; c++)
-		putchar(c);
+	for (j = 'a' ; j <= 'f' ; j++)
+		putchar(j);
 	putchar('\n');
 	return (0);
 }
