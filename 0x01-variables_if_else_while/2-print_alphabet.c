827730e51564591e3bda31989a9ae6df417d5c6f
Nqobile1012 <buthelezinqobile45g@gmail.com>
2023-08-11 06:35:23 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+		putchar(c);
+		c++;
+		}
+	putchar('\n');
+	return (0);
+}
