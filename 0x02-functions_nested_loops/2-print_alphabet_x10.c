2c845365f2c63b4ac6f9f82e51eab2cb7a68ff30
Nqobile1012 <buthelezinqobile45g@gmail.com>
2023-08-15 05:09:44 -0700

    Alphabets x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet x10
+ * Return: must be 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char j;
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 'a' ; j <= 'z' ; j++)
+			_putchar(j);
+		_putchar('\n');
+	}
+}
