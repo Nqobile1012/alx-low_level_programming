bdebd77941b459eb2f7f56485effe3b93655070d
Nqobile1012 <buthelezinqobile45g@gmail.com>
2023-08-21 05:08:46 -0700

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function taht resets value of pointer to 98
+ * @n: the pointer to reset to 98
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
