cd8b98c7b47afb19c5d836fe7697ca880c39dd1d
Nqobile1012 <buthelezinqobile45g@gmail.com>
2023-08-21 06:43:26 -0700

    Puts 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: input string
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (count % 2 == 0)
+			_putchar(str[count]);
+		count++;
+	}
+}
